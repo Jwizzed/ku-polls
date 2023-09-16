d7b48a24adb2aac727dca110b055bcd40a8523a5
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-09-16 10:11:39 +0700

    added some tests in test_auth_user

--- polls/tests/test_auth_user.py
+++ polls/tests/test_auth_user.py
@@ -8,28 +8,24 @@ from mysite import settings
 
 
 class UserAuthTest(django.test.TestCase):
+    """Tests of authentication."""
 
     def setUp(self):
-        # superclass setUp creates a Client object and initializes test database
         super().setUp()
         self.username = "testuser"
         self.password = "FatChance!"
         self.user1 = User.objects.create_user(
                          username=self.username,
                          password=self.password,
-                         email="testuser@nowhere.com"
                          )
         self.user1.first_name = "Tester"
         self.user1.save()
-        # we need a poll question to test voting
         q = Question.objects.create(question_text="First Poll Question")
         q.save()
-        # a few choices
         for n in range(1,4):
             choice = Choice(choice_text=f"Choice {n}", question=q)
             choice.save()
         self.question = q
-
 
     def test_logout(self):
         """A user can logout using the logout url.
@@ -40,37 +36,25 @@ class UserAuthTest(django.test.TestCase):
         and then redirected to the login page.
         """
         logout_url = reverse("logout")
-        # Authenticate the user.
-        # We want to logout this user, so we need to associate the
-        # user user with a session.  Setting client.user = ... doesn't work.
-        # Use Client.login(username, password) to do that.
-        # Client.login returns true on success
-        self.assertTrue( 
+        self.assertTrue(
               self.client.login(username=self.username, password=self.password)
                        )
         # visit the logout page
         response = self.client.get(logout_url)
         self.assertEqual(302, response.status_code)
-        
-        # should redirect us to where? Polls index? Login?
         self.assertRedirects(response, reverse(settings.LOGOUT_REDIRECT_URL))
 
 
     def test_login_view(self):
         """A user can login using the login view."""
         login_url = reverse("login")
-        # Can get the login page
         response = self.client.get(login_url)
         self.assertEqual(200, response.status_code)
-        # Can login using a POST request
-        # usage: client.post(url, {'key1":"value", "key2":"value"})
-        form_data = {"username": "testuser", 
+        form_data = {"username": "testuser",
                      "password": "FatChance!"
                     }
         response = self.client.post(login_url, form_data)
-        # after successful login, should redirect browser somewhere
         self.assertEqual(302, response.status_code)
-        # should redirect us to the polls index page ("polls:index")
         self.assertRedirects(response, reverse(settings.LOGIN_REDIRECT_URL))
 
 
@@ -83,15 +67,27 @@ class UserAuthTest(django.test.TestCase):
           or I receive a 403 response (FORBIDDEN)
         """
         vote_url = reverse('polls:vote', args=[self.question.id])
-
-        # what choice to vote for?
         choice = self.question.choice_set.first()
-        # the polls detail page has a form, each choice is identified by its id
         form_data = {"choice": f"{choice.id}"}
         response = self.client.post(vote_url, form_data)
-        # should be redirected to the login page
-        self.assertEqual(response.status_code, 302)  # could be 303
-        # Fixed the redirection URL to include the 'next' parameter
+        self.assertEqual(response.status_code, 302)
         login_with_next = f"{reverse('login')}?next={vote_url}"
         self.assertRedirects(response, login_with_next)
 
+    def test_failed_login_invalid_password(self):
+        """A user cannot login with an incorrect password."""
+        login_url = reverse("login")
+        form_data = {"username": self.username, "password": "WrongPassword!"}
+        response = self.client.post(login_url, form_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,
+                            "Please enter a correct username and password.")
+
+    def test_failed_login_nonexistent_username(self):
+        """A user cannot login with a non-existent username."""
+        login_url = reverse("login")
+        form_data = {"username": "nonexistentuser", "password": "AnyPassword!"}
+        response = self.client.post(login_url, form_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,
+                            "Please enter a correct username and password.")
