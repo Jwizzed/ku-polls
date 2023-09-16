c1c5289a19f0c83b6b6e97e742bf1176219faf24
Krittin SETDHAVANICH <krittin.set@ku.th>
2023-09-16 11:19:39 +0700

    Added Logging User

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,3 +130,41 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Logging configuration (Both console and file)
+# https://docs.djangoproject.com/en/4.2/howto/logging/
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'pollsDebug.log',
+            'formatter': 'verbose',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'polls': {
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
--- polls/views.py
+++ polls/views.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Question, Choice, Vote
+import logging
 
 
 class IndexView(generic.ListView):
@@ -126,27 +127,46 @@ def vote(request, question_id):
     :return: Redirects to results page or detail page with error message
     """
     question = get_object_or_404(Question, pk=question_id)
+    ip = get_client_ip(request)
+    this_user = request.user
+    logger = logging.getLogger("polls")
+    logger.info(f"{this_user} logged in from {ip}")
 
     if not question.can_vote():
         messages.error(request, f"Poll number {question.id} "
                                 f"is not available to vote")
+        logger.warning(f"{this_user} failed to vote for {question}"
+                          f" from {ip}")
         return redirect("polls:index")
 
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
+        logger.warning(f"{this_user} failed to vote for {question}"
+                       f" from {ip}")
         return render(request, "polls/detail.html", {
             "question": question,
             "error_message": "Please select a choice!",
         })
 
-    this_user = request.user
     try:
         curr_vote = Vote.objects.get(user=this_user, choice__question=question)
         curr_vote.choice = selected_choice
     except Vote.DoesNotExist:
         curr_vote = Vote(user=this_user, choice=selected_choice)
     curr_vote.save()
+
+    logger.info(f"{this_user} voted for {question} from {ip}")
     messages.success(request, f"Your vote for {question} has been recorded.")
     return HttpResponseRedirect(reverse("polls:results",
                                         args=(question.id,)))
+
+
+def get_client_ip(request):
+    """Get the visitor’s IP address using request headers."""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
